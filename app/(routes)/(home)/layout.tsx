9538e212052fc4118ac752d7e76b377764b5b7a8
elias1991xNataly <131762074+elias1991xNataly@users.noreply.github.com>
2025-05-03 20:28:44 +0200

    Finish SIdeBard

---
+++ app/(routes)/(home)/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import AdminSidebar from '@/components/Shared/AdminSidebar/AdminSidebar';
+
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <SidebarProvider>
+            <AdminSidebar />
+            <main className='w-full bg-[#F3F3F1]'>
+                <div className='px-3'>
+                    <SidebarTrigger />
+                </div>
+                {children}
+            </main>
+        </SidebarProvider>
+    )
+}
+
+
---
+++ app/(routes)/(home)/page.tsx
@@ -0,0 +1,12 @@
+
+//import { Button } from "@/components/ui/button";
+//import { toast } from "sonner";
+export default function page() {
+
+    return (
+        <div >
+
+            <p>page</p>
+        </div>
+    );
+}
